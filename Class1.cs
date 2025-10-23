54f538d6ee12fb56696000e28d89ac4aef762f3f
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-10-23 16:44:10 +0530

    stack working

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace CollectionsLibraryDemo
+{
+    public class StackWorking
+    {
+
+        static Stack s=new Stack();
+
+        static StackWorking()
+        {
+            s.Push(100);
+            s.Push(200);
+            s.Push(300);
+        
+        }
+
+        public void AddToStack(object o)
+        { 
+        s.Push(o);
+        }
+
+        public object[] ConvertToArray()
+        {
+            object[] obj=s.ToArray();
+            
+            return obj;
+
+        }
+
+
+        public object[] CopyAll()
+        {
+            object[] o=new object[s.Count];
+            s.CopyTo(o, 0);
+            return o;
+        }
+
+
+
+
+        public void RemoveFromStack()
+        {
+            s.Pop();
+        }
+
+        public void Display()
+        {
+            foreach (var item in s)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
